576d1dbc355515b9365151ece72da568bb81817a
Abhishek G Solanki <abhisheks8676@gmail.com>
2026-01-14 23:24:44 +0530

    Added arrays problem

--- Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/easy/MaximumSubarrayKadaneAlgorithm.java
+++ Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/easy/MaximumSubarrayKadaneAlgorithm.java
@@ -1,4 +1,19 @@
 package org.leetcode.arrays.easy;
 
 public class MaximumSubarrayKadaneAlgorithm {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int total = 0;
+        int result = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (total < 0)
+                total = 0;
+
+            total += nums[i];
+
+            if (total > result)
+                result = total;
+        }
+        System.out.println(result);
+    }
 }
