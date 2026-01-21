91afa736affd1377b463465cc0eb8d51f423c280
Abhishek G Solanki <abhisheks8676@gmail.com>
2026-01-21 15:38:59 +0530

    Solved anagram problem

--- Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/day2/FindAllAnagramsInAString.java
+++ Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/day2/FindAllAnagramsInAString.java
@@ -1,4 +1,20 @@
 package org.leetcode.arrays.day2;
 
+import java.util.*;
+
 public class FindAllAnagramsInAString {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            map.computeIfAbsent(key, V -> new ArrayList<>()).add(word);
+        }
+    }
 }
