9ab9f0ce37c5d432caa0994caad2c11086f28a2f
Abhishek G Solanki <abhisheks8676@gmail.com>
2026-01-22 07:57:10 +0530

    Solved 2 problems

--- Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/day2/MaximumSubarraySumWithOneDeletion.java
+++ Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/day2/MaximumSubarraySumWithOneDeletion.java
@@ -1,4 +1,21 @@
 package org.leetcode.arrays.day2;
 
 public class MaximumSubarraySumWithOneDeletion {
+
+    public static void main(String[] args) {
+        int[] arr = {1, -1, -1, 3};
+
+
+        int result = arr[0];
+        int nodelete = arr[0];
+        int onedelete = arr[0];
+
+        for(int i =1 ; i < arr.length ; i++){
+            onedelete = Math.max(onedelete +arr[i], nodelete);
+
+            nodelete = Math.max(nodelete + arr[i], arr[i]);
+
+            result = Math.max(nodelete,Math.max(onedelete,result));
+        }
+    }
 }
--- Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/day2/SubarraySumEqualsK.java
+++ Leetcode/Arrays/LeetCodeArrays/src/main/java/org/leetcode/arrays/day2/SubarraySumEqualsK.java
@@ -1,4 +1,29 @@
 package org.leetcode.arrays.day2;
 
+import java.util.ArrayList;
+import java.util.*;
+
 public class SubarraySumEqualsK {
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1};
+        int k = 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);   // base case
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+
+    }
 }
